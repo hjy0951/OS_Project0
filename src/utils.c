e7515b9c9d8c21ab87d65b68fc8222e1ff439a30
Heo Joon Young <hjy0951@ajou.ac.kr>
2018-03-29 08:01:25 +0900

    aa

--- src/utils.c
+++ src/utils.c
@@ -5,13 +5,13 @@
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-	char* argvTemp = strtok(command, " \n");
+	char* argvTemp = strtok(command, " \n\t");
 	*argv = (char**)malloc(sizeof(char*)*10);
 
 	for(*argc = 0; argvTemp != NULL ; (*argc)++) {
 		(*argv)[*argc] = (char*)malloc(sizeof(char));
 		strcpy((*argv)[*argc], argvTemp);
-		argvTemp = strtok(NULL," \n");
+		argvTemp = strtok(NULL," \n\t");
 	}
 
 	if(*argc == 0){
@@ -20,7 +20,7 @@ void mysh_parse_command(const char* command,
 		(*argc)++;
 	}
 
-	// printf("argc : %d\n", *argc);
-	// for(int i = 0; i < *argc; i++) 
-	// 	printf("argv[%d] : %s\n", i, (*argv)[i]);
+	printf("argc : %d\n", *argc);
+	for(int i = 0; i < *argc; i++) 
+		printf("argv[%d] : %s\n", i, (*argv)[i]);
 }
