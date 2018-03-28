4fd1fdef7ca675be0e237ef85d7b809bc04b2d15
Heo Joon Young <hjy0951@ajou.ac.kr>
2018-03-29 08:04:00 +0900

    tap

--- src/utils.c
+++ src/utils.c
@@ -20,7 +20,7 @@ void mysh_parse_command(const char* command,
 		(*argc)++;
 	}
 
-	printf("argc : %d\n", *argc);
-	for(int i = 0; i < *argc; i++) 
-		printf("argv[%d] : %s\n", i, (*argv)[i]);
+	// printf("argc : %d\n", *argc);
+	// for(int i = 0; i < *argc; i++) 
+	// 	printf("argv[%d] : %s\n", i, (*argv)[i]);
 }
